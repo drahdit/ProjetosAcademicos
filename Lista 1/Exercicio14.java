18d0814577b559fa9f9cfd23b8490dd2876a26b1
drahdit <80552873+drahdit@users.noreply.github.com>
2024-09-17 11:49:26 -0300

    Update Exercicio14.java

--- Lista 1/Exercicio14.java
+++ Lista 1/Exercicio14.java
@@ -1,3 +1,7 @@
+/*Faça um algoritmo que leia uma temperatura em graus Celsius e apresente-a 
+convertida em graus Fahrenheit. A fórmula de conversão é: F = (9 * C + 160) / 5, na 
+qual F é a temperatura em Fahrenheit C é a temperatura em Celsius.*/
+	
 import javax.swing.JOptionPane;
 
 public class Exercicio14 {
