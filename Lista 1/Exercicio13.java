74a2e837934db3b6af5484003c5c2f4afd6303d3
Richard <80552873+drahdit@users.noreply.github.com>
2024-11-07 14:32:09 -0300

    Alteração de teste

--- Lista 1/Exercicio13.java
+++ Lista 1/Exercicio13.java
@@ -21,6 +21,8 @@ public class Exercicio13 {
 		Double b = Double.parseDouble(JOptionPane.showInputDialog("Qual o valor da hora trabalhada?"));
 		Double c = Double.parseDouble(JOptionPane.showInputDialog("Qual o percentual de desconto?"));
 		
+		int teste = 1;
+		
 		Double sb = a * b;
 		Double td = (c / 100)*sb;
 		Double sl = sb - td;
